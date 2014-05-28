16bf7d54dabd66ec4757387a6f9c9a992e3ee7e8
Devolvera <donovan.olvera@gmail.com>
2014-05-28 13:19:23 -0700

    explain heightstuff code

--- js/heightstuff.js
+++ js/heightstuff.js
@@ -1,4 +1,12 @@
 // heightstuff.js
+/*
+  I wanted the color reference divs to cover the user's window on load,
+  so this code does that. It takes the height (width was optional) of
+  the user's viewport, and then divides by 6 (the amount of divs) to
+  get the height that each div should be. each div's height is then set
+  to that number so it covers the user's window completely, but each div is
+  equal in height.
+*/
 
 $(document).ready(function() {
 
